84f5077416c27996e85530624218c9670bd49c9a
Clement BOIRON <cboiron@e2r1p10.42.fr>
2018-03-20 03:08:21 +0100

    live, ld et st

---
+++ VM/src/ld.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ld.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cboiron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/20 01:57:02 by cboiron           #+#    #+#             */
+/*   Updated: 2018/03/20 02:54:29 by cboiron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void	ld(t_vm *vm, int *i)
+{
+	(*i)++;
+	if (vm->arena[*i] == 144)
+	{
+		*i += 4;
+		//lire les 4 octets, on a le direct
+	}
+	else if (vm->arena[*i] == 208)
+	{
+		*i += 2;
+		//lire les 2 octets, on a l'index
+	}
+	*i += 1;
+	//lire l'octet suivant, on a le registre
+	//copier premier param dans registre
+	//si premier param = 0 alors carry = 1
+	
+}
---
+++ VM/test.s
@@ -0,0 +1,6 @@
+.name       "Test"
+.comment    "Night at 42"
+
+	st      r1,42
+	st      r1,r2
+	
