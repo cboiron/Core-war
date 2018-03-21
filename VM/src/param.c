33dd03f073bc9952e6a158ba1f8788ab0d2a7ec4
Clement BOIRON <cboiron@e3r9p10.42.fr>
2018-03-21 05:42:51 +0100

    norme param.c

--- VM/src/param.c
+++ VM/src/param.c
@@ -6,7 +6,7 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 16:32:14 by abrichar          #+#    #+#             */
-/*   Updated: 2018/03/21 04:31:55 by cboiron          ###   ########.fr       */
+/*   Updated: 2018/03/21 05:42:29 by cboiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,20 @@ int ft_isnumber(char *str)
 	return (1);
 }
 
+void	champs(t_vm *vm, char *arg)
+{
+	if ((match(arg, "*.cor")))
+	{
+		if ((vm->fd = open(arg, O_RDONLY)) >= 0 && vm->nbr_next < 3)
+		{
+			read_champ(vm, vm->nbr_next);
+			close(vm->fd);
+		}
+		else
+			usage();
+	}
+}
+
 int get_param(char **av, t_vm *vm, int ac)
 {
 	int i;
@@ -44,31 +58,18 @@ int get_param(char **av, t_vm *vm, int ac)
 	i = 1;
 	while (av[i])
 	{
-		if ((match(av[i], "*.cor")))
+		champs(vm, av[i]);
+		if (((ft_strcmp(av[i], "-n") == 0) ||
+					ft_strcmp(av[i], "-dump") == 0) && ac >= i + 2 &&
+				(ft_isnumber(av[++i]) == 0))
 		{
-			if ((vm->fd = open(av[i], O_RDONLY)) >= 0 && vm->nbr_next < 3)
-			{
-				read_champ(vm, vm->nbr_next);
-				close(vm->fd);
-			}
-			else
-				usage();
-		}
-		else if (((ft_strcmp(av[i], "-n") == 0) ||
-				 ft_strcmp(av[i], "-dump") == 0) && ac >= i + 2)
-		{
-			if (ft_isnumber(av[++i]) == 0)
-			{
-				ft_putendl("Non présence d'un nombre après -n ou -dump");
-				usage();
-			}
-			else if (ft_strcmp(av[i], "-n") == 0)
-				param_n(vm, ft_atoi(av[++i]), av, &i);
-			else
-				vm->dump_cycle = ft_atoi(av[i]);
-		}
+		if (ft_strcmp(av[i], "-n") == 0)
+			param_n(vm, ft_atoi(av[++i]), av, &i);
+		else if (ft_strcmp(av[i], "-dump") == 0 && vm->dump_cycle == -1)
+			vm->dump_cycle = ft_atoi(av[i]);
 		else
 			return (0);
+		}
 		i++;
 	}
 	if (vm->nbr_next == 0)
