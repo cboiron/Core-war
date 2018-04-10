180017e99da91195dfeeb2c1679ae1b1afe42735
abrichar <abrichar@student.42.fr>
2018-04-10 18:18:41 +0200

    MAJ

---
+++ libft/src/ft_epur_str.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_epur_str.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eliajin <abrichar@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/10 17:40:58 by eliajin           #+#    #+#             */
+/*   Updated: 2018/04/10 18:16:42 by eliajin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *ft_epur_str(char *str)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	j = 0;
+	if ((dest = (char*)malloc(sizeof(char*) * ft_strlen(str))) == NULL)
+		 return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+		{
+			while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
+				i++;
+			if (j != 0 && str[i] != '\0')
+				dest[j++] = ' ';
+		}
+		dest[j++] = str[i++];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
