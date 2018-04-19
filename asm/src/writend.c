dae964eb05afcbd4eeb790b32095780cefcca808
Antoine BRICHARD <abrichar@e3r13p1.42.fr>
2018-04-20 01:03:51 +0200

    Début_opcode

--- asm/src/writend.c
+++ asm/src/writend.c
@@ -6,31 +6,48 @@
 /*   By: eliajin <abrichar@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 22:25:42 by eliajin           #+#    #+#             */
-/*   Updated: 2018/04/19 21:36:42 by abrichar         ###   ########.fr       */
+/*   Updated: 2018/04/20 01:02:05 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-/*static void	    write_file(int fd, t_asm *env)
+static void		write_octetcodage()
 {
-	write(fd, &env->magic, sizeof(env->magic));
-	while(env->buff_write->next)
-	{
-		ft_putstr("je passe la\n");
-		env->buff_write = env->buff_write->next;
-		write(fd, env->buff_write->content, sizeof(env->buff_write->content));
-		}
-}*/
+	//regarder par rapport aux param suivants
+}
+
+static void		write_opcode(char *name, int fd)
+{
+	t_op *tab_op;
+	int i;
+	int nbr;
+
+	i = -1;
+	tab_op = get_ops();
+	while (ft_strcmp(tab_op[++i].name, name) != 0);
+	write(fd, &tab_op[i].opcode, sizeof(tab_op[i].opcode) / 4);
+}
+
+static void	    write_body(int fd, t_asm *env)
+{
+	char **split;
+	int i;
+
+	i = 0;
+	split = ft_strsplit(env->buff->content, ' ');
+	if (split[i][ft_strlen(split[0]) - 1] == LABEL_CHAR)
+		i++;
+	write_opcode(split[i], fd);
+	write_octetcodage();////////////je suis la
+}
 
 void			ft_write_out(t_asm *env)
 {
-	int			fd;
-
-	fd = open(env->champ_name, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	env->fd = open(env->champ_name, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	env->header->prog_size = reverse_bits(env->header->prog_size) << 24;
-//	write_file(fd, env);
 	write(fd, env->header, sizeof(t_header));
-//	write_body(fd, env);
+	ft_printf("test : %s\n",env->buff->content);
+	write_body(fd, env);
 	close(fd);
 }
