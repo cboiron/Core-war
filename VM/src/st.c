942d568c081475ae567230d4f0333766d6723a4d
Clement BOIRON <cboiron@e3r12p5.42.fr>
2018-04-28 18:14:40 +0200

    fix st

--- VM/src/st.c
+++ VM/src/st.c
@@ -6,7 +6,7 @@
 /*   By: cboiron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 05:17:20 by cboiron           #+#    #+#             */
-/*   Updated: 2018/04/28 17:50:30 by cboiron          ###   ########.fr       */
+/*   Updated: 2018/04/28 18:13:40 by cboiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,11 @@ void	st(t_vm *vm, t_proc *proc)
 	if (proc->parametres_types[1] == REG)
 		arg2 = get_reg(vm, &(proc->pc));
 	else
-		arg2 = get_ind(vm, &(proc->pc));
+		arg2 = (short)get_ind(vm, &(proc->pc));
 	if (!is_reg(arg1) || (proc->parametres_types[1] == REG && !is_reg(arg2)))
 		return ;
 	printf("arg2 = %d\n", arg2);
+	printf("arg2 = %d\n", arg2 % IDX_MOD);
 	pc_debut = proc->pc;
 	if (proc->parametres_types[1] == REG)
 		proc->reg[arg2] = proc->reg[arg1];
