27818b66e353898c03a5aa7d5ca412d1ef3ffd11
Arnaud DURAND <ardurand@e2r1p4.42.fr>
2018-04-29 07:34:38 +0200

    test ld

---
+++ VM/simplychamp/testld.s
@@ -0,0 +1,19 @@
+.name	"testld"
+.comment	"many test for ld"
+
+ld 101, r1
+ld 102, r2
+ld 103, r3
+ld 104, r4
+ld 105, r5
+ld 106, r6
+ld 107, r7
+ld 108, r8
+ld 109, r9
+ld 110, r10
+ld 111, r11
+ld 112, r12
+ld 113, r13
+ld 114, r14
+ld 115, r15
+ld 116, r16
