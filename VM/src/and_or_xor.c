e7f97f22650336e062b1f540f44ca5e78617a441
Clement BOIRON <cboiron@e2r1p2.42.fr>
2018-04-29 04:31:11 +0200

    st a l'air ok

--- VM/src/and_or_xor.c
+++ VM/src/and_or_xor.c
@@ -6,7 +6,7 @@
 /*   By: cboiron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/21 04:04:22 by cboiron           #+#    #+#             */
-/*   Updated: 2018/04/28 14:31:14 by cboiron          ###   ########.fr       */
+/*   Updated: 2018/04/29 03:03:24 by cboiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,41 @@ void	choose_op(int *arg, t_proc *proc)
 
 void	param_op(t_vm *vm, t_proc *proc, int *arg)
 {
+	int	pc;
+
+	proc->pc++;
+	pc = proc->pc + 1;
+	printf("case = %d\n", vm->arena[pc]);
+	printf("pc = %d\n", pc);
 	if (proc->parametres_types[0] == REG)
 	{
-		arg[0] = get_reg(vm, &(proc->pc));
+		arg[0] = get_reg(vm, &pc);
 		arg[0] = proc->reg[arg[0] - 1];
 	}
 	else if (proc->parametres_types[0] == INDIRECT)
-		arg[0] = get_ind(vm, &(proc->pc));
+		arg[0] = get_ind(vm, &pc);
 	else if (proc->parametres_types[0] == DIRECT)
-		arg[0] = get_dir(vm, &(proc->pc), proc->instruction);
+		arg[0] = get_dir(vm, &pc, proc->instruction);
+	printf("pc arg 1 =  %d\n", pc);
 	if (proc->parametres_types[1] == REG)
 	{
-		arg[1] = get_reg(vm, &(proc->pc));
+		arg[1] = get_reg(vm, &pc);
 		arg[1] = proc->reg[arg[1] - 1];
 	}
 	else if (proc->parametres_types[1] == INDIRECT)
-		arg[1] = get_ind(vm, &(proc->pc));
+		arg[1] = get_ind(vm, &pc);
 	else if (proc->parametres_types[1] == DIRECT)
-		arg[1] = get_dir(vm, &(proc->pc), proc->instruction);
-	arg[2] = get_reg(vm, &(proc->pc));
+		arg[1] = get_dir(vm, &pc, proc->instruction);
+	printf("pc arg 2 =  %d\n", pc);
+	arg[2] = get_reg(vm, &pc);
 	if ((proc->parametres_types[0] == REG && !is_reg(arg[0])) ||
 			(proc->parametres_types[1] == REG && !is_reg(arg[1])) ||
 			(proc->parametres_types[2] == REG && !is_reg(arg[2])))
+	{
+	printf("pc = %d\n", pc);
 		return ;
+	}
 	choose_op(arg, proc);
+	proc->pc = pc;
+	printf("pc = %d\n", pc);
 }
--- VM/src/st.c
+++ VM/src/st.c
@@ -6,7 +6,7 @@
 /*   By: cboiron <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 05:17:20 by cboiron           #+#    #+#             */
-/*   Updated: 2018/04/29 02:21:40 by cboiron          ###   ########.fr       */
+/*   Updated: 2018/04/29 04:30:28 by cboiron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ void	st(t_vm *vm, t_proc *proc)
 	if (!is_reg(arg1) || (proc->parametres_types[1] == REG && !is_reg(arg2)))
 		return ;
 	printf("arg2 = %d\n", arg2);
+	arg2 %= IDX_MOD;
 	printf("arg2 = %d\n", arg2 % IDX_MOD);
-	pc_debut = proc->pc;
 	if (proc->parametres_types[1] == REG)
 		proc->reg[arg2] = proc->reg[arg1];
 	else
 	{
-		write_in_memory(vm, proc->reg[arg1 - 1], pc_debut + (arg2 % IDX_MOD));
+		write_in_memory(vm, proc->reg[arg1 - 1], pc_debut + mod(arg2, MEM_SIZE));
 	}
 }
