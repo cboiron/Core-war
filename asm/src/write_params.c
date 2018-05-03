ecac7c8404ddb51c9dd0398b254a91a152e6d91f
Antoine BRICHARD <abrichar@e3r9p2.42.fr>
2018-05-04 00:29:18 +0200

    error

--- asm/src/write_params.c
+++ asm/src/write_params.c
@@ -1,75 +1,84 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   write_label.c                                      :+:      :+:    :+:   */
+/*   write_params.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/03 06:31:50 by abrichar          #+#    #+#             */
-/*   Updated: 2018/05/04 00:01:23 by abrichar         ###   ########.fr       */
+/*   Created: 2018/04/20 16:46:23 by abrichar          #+#    #+#             */
+/*   Updated: 2018/05/04 00:26:12 by ardurand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-static unsigned int	write_label3(t_parsing *tmp, unsigned int size_to_here,
-		unsigned int pos)
+static void				write_dir(char *dir, t_asm *env, t_op actual,
+						unsigned int size_to_here)
 {
-	while (tmp)
+	int	to_write;
+
+	if ((search_char(dir, LABEL_CHAR)) == -1)
 	{
-		if (tmp->size_to_here == size_to_here)
-			break ;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		tmp = tmp->next;
+		to_write = ft_atoi(ft_strsub(dir, 1,
+									ft_strlen(dir)));
+		if (actual.opcode >= 9 && actual.opcode != 13 && actual.opcode <= 15)
+			ft_putshort_fd(to_write, env->fd);
+		else
+			ft_putint_fd(to_write, env->fd);
 	}
-	pos += tmp->size;
-	return (pos);
-}
-
-unsigned int		write_label2(t_parsing *tmp, char *to_search,
-		unsigned int size_to_here, t_asm *env)
-{
-	unsigned	intpos;
-	char		*content;
-
-	while (tmp)
-	{
-		while (tmp->label != 1 && tmp->next != NULL)
-			tmp = tmp->next;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		content = ft_strsub(tmp->content, 0, ft_strlen(tmp->content) - 1);
-		if (ft_strcmp(content, to_search) == 0)
-			break ;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		tmp = tmp->next;
-	}
-	pos = (tmp->size_to_here - size_to_here);
-	tmp = env->buff;
-	pos = write_label3(tmp, size_to_here, pos);
-	return (pos);
-}
-
-void				write_label(char *dir, t_op actual, t_asm *env,
-		unsigned int size_to_here)
-{
-	unsigned int	pos;
-	t_parsing		*tmp;
-	char			*to_search;
-	unsigned int	max;
-
-	max = -1;
-	to_search = ft_strsub(dir, 2, ft_strlen(dir));
-	tmp = env->buff;
-	pos = write_label2(tmp, to_search, size_to_here, env);
-	if (pos == max)
-		pos = 0;
-	pos -= ft_atoi(to_search);
-	if (actual.opcode >= 9 && actual.opcode <= 15 && actual.opcode != 13)
-		ft_putshort_fd(pos, env->fd);
 	else
-		ft_putint_fd(pos, env->fd);
+		write_label(dir, actual, env, size_to_here);
+}
+
+static void				write_ind(char *ind, t_asm *env,
+								unsigned int size_to_here)
+{
+	int			to_write;
+	char		*to_search;
+	t_parsing	*tmp;
+
+	to_write = 0;
+	to_search = NULL;
+	tmp = NULL;
+	if (ind[0] != ':')
+		to_write = ft_atoi(ft_strsub(ind, 0,
+									ft_strlen(ind)));
+	else
+	{
+		tmp = env->buff;
+		to_search = ft_strsub(ind, 1, ft_strlen(ind) - 1);
+		to_write = write_label2(tmp, to_search, size_to_here, env);
+	}
+	ft_putshort_fd(to_write, env->fd);
 	free(to_search);
+	free(tmp);
+}
+
+void					write_params(t_asm *env, char *split, t_op actual,
+					unsigned int size_to_here)
+{
+	char			**splited;
+	int				i;
+	int				to_write;
+
+	splited = ft_strsplit(split, SEPARATOR_CHAR);
+	clear_split(splited);
+	i = -1;
+	to_write = 0;
+	while (++i < tab_len(splited))
+	{
+		if (check_param(splited[i]) == REG_CODE)
+		{
+			to_write = ft_atoi(ft_strsub(splited[i], 1,
+										ft_strlen(splited[i])));
+			write(env->fd, &to_write, 1);
+		}
+		else if (check_param(splited[i]) == IND_CODE)
+		{
+			write_ind(splited[i], env, size_to_here);
+		}
+		else
+			write_dir(splited[i], env, actual, size_to_here);
+	}
+	free(splited);
 }
