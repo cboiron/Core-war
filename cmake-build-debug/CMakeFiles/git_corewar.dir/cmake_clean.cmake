ad186521998cccd078fd936c3e9948e004be6468
Antoine BRICHARD <abrichar@e3r9p2.42.fr>
2018-05-04 00:16:04 +0200

    flood

--- cmake-build-debug/CMakeFiles/git_corewar.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/git_corewar.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/git_corewar.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for git_corewar.
-# This may be replaced when dependencies are built.
