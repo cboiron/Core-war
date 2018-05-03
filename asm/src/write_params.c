4a1b053dd7f77ed55495a651d81cb6f7fd4ff24a
Antoine BRICHARD <abrichar@e3r13p4.42.fr>
2018-05-03 06:39:45 +0200

    maj_norme

--- asm/src/write_params.c
+++ asm/src/write_params.c
@@ -6,74 +6,11 @@
 /*   By: abrichar <abrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 16:46:23 by abrichar          #+#    #+#             */
-/*   Updated: 2018/05/01 23:49:19 by abrichar         ###   ########.fr       */
+/*   Updated: 2018/05/03 06:33:14 by abrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-static unsigned int		write_label2(t_parsing *tmp, char *to_search,
-									unsigned int size_to_here, t_asm *env)
-{
-	unsigned int	pos;
-	char			*content;
-
-	pos = 0;
-	while (tmp)
-	{
-		while (tmp->label != 1 && tmp->next != NULL)
-			tmp = tmp->next;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		content = ft_strsub(tmp->content, 0, ft_strlen(tmp->content) - 1);
-		if (ft_strcmp(content, to_search) == 0)
-			break ;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		tmp = tmp->next;
-
-	}
-	pos = (tmp->size_to_here - size_to_here);
-	tmp = env->buff;
-	while (tmp)
-	{
-		if (tmp->size_to_here == size_to_here)
-			break ;
-		if (tmp->next == NULL)
-			msg_error(NO_LABEL, 0);
-		tmp = tmp->next;
-	}
-	pos += tmp->size;
-	return (pos);
-}
-
-//vérifier qu'il n'y ai pas + ou moins
-static void				write_label(char *dir, t_op actual, t_asm *env,
-							unsigned int size_to_here)
-{
-	unsigned int	pos;
-	t_parsing		*tmp;
-	char			*to_search;
-	unsigned int	max;
-	int sign;
-
-	sign = 0;
-	max = -1;
-	if ((sign = search_char(dir, '+') <= 0) || ((sign = search_char(dir, '-')) <= 0))
-		sign = ft_strlen(dir);
-	to_search = ft_strsub(dir, 2, sign);
-	tmp = env->buff;
-	pos = write_label2(tmp, to_search, size_to_here, env);
-	if (pos == max)
-		pos = 0;
-	if (sign != (int)ft_strlen(dir))
-		to_search = ft_strsub(dir, sign, ft_strlen(dir));
-	pos -= ft_atoi(to_search);
-	if (actual.opcode >= 9 && actual.opcode <= 15 && actual.opcode != 13)
-		ft_putshort_fd(pos, env->fd);
-	else
-		ft_putint_fd(pos, env->fd);
-}
 
 static void				write_dir(char *dir, t_asm *env, t_op actual,
 						unsigned int size_to_here)
